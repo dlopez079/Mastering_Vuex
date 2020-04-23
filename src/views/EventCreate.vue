6bdd80fa596277f5453de14f8891cd8ef2452bf3
Dlopez079 <dlopez@lopezdavid.dev>
2020-04-23 15:30:23 -0400

    Added function that takes in state and returns the property of the state we want, state.user.name and state.categories. Imported mapState from vuex.

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,19 @@
 <template>
-  <h1>Events Create</h1>
+  <div>
+    <h1>Create an Event, {{ userName }}</h1>
+    <p>This event was created by {{ userName }}.</p>
+  </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+    computed: mapState({
+        userName: state => state.user.name,
+        categories: state => state.categories
+    })
+}
+</script>
+
+<style></style>
