6a0a0e59290f4b58991db5c1deea78fd54715dc6
Dlopez079 <dlopez@lopezdavid.dev>
2020-04-24 16:41:59 -0400

    I changed the computed property to search for the getter that I just created in the store rather than the state.

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -2,6 +2,7 @@
   <div>
     <h1>Create an Event, {{ user.name }}</h1>
     <p>This event was created by {{ user.id }}.</p>
+    <p>This are {{ catLength }} categories.</p>
     <ul>
       <li v-for="cat in categories" :key="cat">{{ cat }}</li>
     </ul>
@@ -14,7 +15,7 @@ import { mapState } from 'vuex'
 export default {
   computed: {
     catLength() {
-      return this.$store.categories.length
+      return this.$store.getters.catLength
     },
     ...mapState(['user', 'categories'])
   }
