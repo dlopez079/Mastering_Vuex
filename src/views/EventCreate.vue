31bb58b5b841171213ae406d1e5e3864607e9bc6
Dlopez079 <dlopez@lopezdavid.dev>
2020-04-27 17:30:46 -0400

    We use the router.push method to view event that we just created.  This will take the user to the event page.

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -103,6 +103,10 @@ export default {
       this.$store
         .dispatch('createEvent', this.event)
         .then(() => {
+          this.$router.push({
+            name: 'event-show',
+            params: { id: this.event.id }
+          })
           this.event = this.createFreshEventObject()
         })
         .catch(() => {
