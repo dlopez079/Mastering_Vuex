2572767dedfe27660301d95cc63f7b2407a26bd8
Dlopez079 <dlopez@lopezdavid.dev>
2020-04-28 15:38:24 -0400

    at the start of this action we call our getters.getEventById(id) with the id of the event we want to display. If it is found, we commit a Mutation, otherwise we go ahead and get that single event from the API.

--- src/store/index.js
+++ src/store/index.js
@@ -53,14 +53,24 @@ export default new Vuex.Store({
           console.log('There was an error:', error.response)
         })
     },
-    fetchEvent({ commit }, id) {
-      EventService.getEvent(id)
-        .then(response => {
-          commit('SET_EVENT', response.data)
-        })
-        .catch(error => {
-          console.log('There was an error:', error.response)
-        })
+    fetchEvent({ commit, getters }, id) {
+      // Send in the getters
+
+      const event = getters.getEventById(id) // See if we already have this event
+
+      if (event) {
+        // If we do, set the event
+        commit('SET_EVENT', event)
+      } else {
+        // If not, get it with the API.
+        EventService.getEvent(id)
+          .then(response => {
+            commit('SET_EVENT', response.data)
+          })
+          .catch(error => {
+            console.log('There was an error:', error.response)
+          })
+      }
     }
   },
   modules: {},
