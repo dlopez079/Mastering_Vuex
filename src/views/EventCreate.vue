ec7041c301272141c6165fe720a005beccaea878
Dlopez079 <dlopez@lopezdavid.dev>
2020-04-30 13:25:09 -0400

    removed catch info so that event.js can catch the error.  This will allow the user to remain on the page to resubmit.

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -80,9 +80,7 @@ export default {
           })
           this.event = this.createFreshEventObject()
         })
-        .catch(() => {
-          console.log('There was a problem creating your event.')
-        })
+        .catch(() => {})
     },
     createFreshEventObject() {
       // eslint-disable-next-line prefer-destructuring
